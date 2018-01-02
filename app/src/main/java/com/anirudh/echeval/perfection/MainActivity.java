4ff55e41ccbaf6f3f802fde5b8d4623590941bd9
mountbicepius <mountbicepius@github.com>
2018-01-02 20:35:17 +0530

    Initial commit

---
+++ app/src/main/java/com/anirudh/echeval/perfection/MainActivity.java
@@ -0,0 +1,13 @@
+package com.anirudh.echeval.perfection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
